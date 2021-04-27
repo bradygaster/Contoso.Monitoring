8af9d8f3a8c6e1b7d5538b62192da35934285b82
bradygaster <bradyg@microsoft.com>
2021-04-26 23:38:30 -0700

    fixed compilation error

--- src/Contoso.Monitoring.Grains/ContosoMonitoringClientService.cs
+++ src/Contoso.Monitoring.Grains/ContosoMonitoringClientService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Contoso.Monitoring.Grains.Interfaces;
 using Microsoft.Extensions.Logging;
 using Orleans;
 
